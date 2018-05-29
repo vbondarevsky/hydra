ba050417a461e4d01a6cdd156abee9448027e592
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2018-05-29 03:30:53 +0300

    Nodes UI

--- hydra/routes.py
+++ hydra/routes.py
@@ -9,6 +9,7 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 def setup_routes(app):
     app.router.add_get("/", index)
     app.router.add_get("/node/{node_id}", node_detail, name="node_detail")
+    app.router.add_get("/node/add", node_detail, name="node_add")
     setup_static_routes(app)
 
 
