155d7633b42595fcfff0f4eb67315707e443aca5
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2018-10-28 23:37:34 +0300

    Запуск на корневом URL

--- front/src/api/Api.js
+++ front/src/api/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
+        // TODO: в прод версии хост указывать не нужно
         baseURL: `http://localhost:8080/api`,
         headers: {
             'Accept': 'application/json',
--- hydra/routes.py
+++ hydra/routes.py
@@ -1,4 +1,7 @@
+import os.path
 import pathlib
+
+import aiohttp
 
 from hydra.handler import NodeHandler
 
@@ -6,9 +9,14 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 
 
 def setup_routes(app):
+    app.router.add_get("/", root_handle)
     app.add_routes(NodeHandler().routes)
     setup_static_routes(app)
 
 
 def setup_static_routes(app):
-    app.router.add_static("/", path=PROJECT_ROOT / "static", name="static")
+    app.router.add_static("/", path=os.path.join(PROJECT_ROOT, "static"), name="static")
+
+
+async def root_handle(request):
+    return aiohttp.web.FileResponse(os.path.join(PROJECT_ROOT, "static", "index.html"))
